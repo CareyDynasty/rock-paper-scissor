35b10d270218283336df6edfcd897abe36b3aff0
CareyDynasty <dynastycarey5@gmail.com>
2022-07-20 10:42:53 +0300

    Game logic stable

--- script.js
+++ script.js
@@ -9,3 +9,35 @@ function computerPlay() {
     const rock = "rock"
     const paper = "paper"
     const scissor = "scissor"
+
+    if (playerSelection === computerSelection) {
+        console.log("It's a Tie, try again")
+        return null
+      }
+  
+      if (playerSelection === rock && computerSelection === paper) {
+        console.log("You Lose! Paper beats Rock")
+        return false
+      } else if (playerSelection === rock && computerSelection === scissor) {
+        console.log("You Win! Rock beats Scissor")
+        return true
+      } else if (playerSelection === paper && computerSelection === rock) {
+        console.log("You Win! Paper beats Rock")
+        return true
+  
+      } else if (playerSelection === paper && computerSelection === scissor) {
+        console.log("You Lose! Scissors beat Paper")
+        return false
+  
+      } else if (playerSelection === scissor && computerSelection === rock) {
+        console.log("You Lose! Rock beats Scissor")
+        return false
+  
+      } else if (playerSelection === scissor && computerSelection === paper) {
+        console.log("You Win!, Scissors beat Paper")
+        return true
+      } else {
+        console.log("Error, Something is wrong")
+        return null
+      }
+    }
